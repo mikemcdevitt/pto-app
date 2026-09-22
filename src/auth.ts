bba8fb7adeaf64761568f4740ad9c441589fbf8a
Michael McDevitt <turnpikemike@gmail.com>
2026-09-22 14:38:23 -0400

    Updating DrizzleAdapter with custom table names

--- src/auth.ts
+++ src/auth.ts
@@ -3,9 +3,15 @@ import Google from "next-auth/providers/google";
 import Nodemailer from "next-auth/providers/nodemailer";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
+import { users, accounts, sessions, verificationTokens } from "@/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
   providers: [
     Google,
     Nodemailer({
